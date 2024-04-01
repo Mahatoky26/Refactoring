9efaa188bcdff904f10118e6938d2e614d824e50
Mahatoky26 <matokyjos26@gmail.com>
2024-04-02 00:00:27 +0300

    Creation de la methode toString dans la classe Humain

--- src/iut/bad/Humain.java
+++ src/iut/bad/Humain.java
@@ -1,58 +1,14 @@
 package iut.bad;
 
 public class Humain {
+    
     private String nom;
     private String prenom;
     private int age;
-    
-    public Humain() {
-        this.nom = "";
-        this.prenom = "";
-        this.age = 0;
-    }
 
     
-    public Humain(String nom, String prenom, int age) {
-        this.nom = nom;
-        this.prenom = prenom;
-        this.age = age;
-    }
-    
-    
-    
-    public String getNom() {
-		return nom;
-	}
-
-
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-
-
-	public String getPrenom() {
-		return prenom;
-	}
-
-
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
-
-
-	public int getAge() {
-		return age;
-	}
-
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-
-
-	public void details() {
-        System.out.println("Nom: " + nom);
-        System.out.println("Prénom: " + prenom);
-        System.out.println("Âge: " + age);
+    @Override
+    public String toString() {
+        return "Nom: " + nom + "\nPrénom: " + prenom + "\nÂge: " + age;
     }
 }
